ea2acb1039e354844a071a3e0b2c64a8262b8de8
Jaime <jaime.yahperez@gmail.com>
2025-12-12 20:37:20 -0800

    barrier included

--- Project-2/sdf.cpp
+++ Project-2/sdf.cpp
@@ -9,6 +9,7 @@
 #include <cstdio>  
 #include <cstdlib>  
 #include <unistd.h> 
+#include <barrier>
 #include "Shapes.h"
 
 /////////////////////////////////////////////////////////////////////////////
